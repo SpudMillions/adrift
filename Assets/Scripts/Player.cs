b71c0d348958222f5b52a09c3377d27bc54b7ba3
spudmillions <joshuaadams18@gmail.com>
2018-09-29 22:56:37 -0400

    fix running animation

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,17 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Animations;
 using UnityEngine;
 using UnityEngine.Serialization;
 using UnityStandardAssets.CrossPlatformInput;
 
 public class Player : MonoBehaviour
 {
+	//config
 	[FormerlySerializedAs("runSpeed")] [SerializeField] private float _runSpeed = 5f;
+	
+	//state
+	private bool isAlive = true;
+	
+	//caches component references
 	private Rigidbody2D _playerRigidBody;
+	private Animator _playerAnimator;
+	
 	// Use this for initialization
-	void Start ()
+	private void Start ()
 	{
 		_playerRigidBody = GetComponent<Rigidbody2D>();
+		_playerAnimator = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
@@ -21,11 +31,17 @@ public class Player : MonoBehaviour
 		FlipSprite();
 	}
 
+	//methods
 	private void Run()
 	{
 		var controlThrow = CrossPlatformInputManager.GetAxis("Horizontal"); //value is between -1 and +1
 		var playerVelocity = new Vector2(controlThrow * _runSpeed, _playerRigidBody.velocity.y);
 		_playerRigidBody.velocity = playerVelocity;
+		
+		//set animation to running if player is moving
+		var playerHasHorizontalSpeed = Mathf.Abs(_playerRigidBody.velocity.x) > Mathf.Epsilon;
+		_playerAnimator.SetBool("Running", playerHasHorizontalSpeed);
+		
 	}
 
 	private void FlipSprite()
